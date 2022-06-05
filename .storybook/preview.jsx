2c73650958b7366ea98289364d0688a71dfd8b83
Luiz Otávio <luizomf@gmail.com>
2022-06-05 10:56:20 -0300

    add husky to postinstall

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../src/styles/global-styles';
 import { theme } from '../src/styles/theme';
-import '../public/fonts/styles.css';
+import '../public/assets/fonts/styles.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
