849423eddedaef81cc20b3e3de87d4b2b3b09f5a
Luiz Otávio <luizomf@gmail.com>
2022-06-05 11:03:01 -0300

    fix fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { AppProps } from 'next/app';
 
-import '../../public/fonts/assets/fonts/styles.css';
+import '../../public/assets/fonts/styles.css';
 import { GlobalStyles } from '../styles/global-styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
